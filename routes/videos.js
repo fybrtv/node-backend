c67be1f1fc15950bba45d663e0a733faa64c5106
Carter Henderson <carterh062@gmail.com>
2015-04-16 15:56:58 -0400

    commented videos.js

--- routes/videos.js
+++ routes/videos.js
@@ -11,75 +11,63 @@ function sendERR(err, res) {
 exports.videoPOST = function(req, res, next) {
     var data = req.body;
     try {
-        series.findOne({
+        series.findOne({ //find series
             _id: data.seriesId
         }, function(err, doc) {
             if (!err) {
-                var score = doc.score;
-                var newVideo = new video({
+                var score = doc.score;//get score for series
+                var newVideo = new video({//create new video file object
                     seriesId: data.seriesId,
                     fileName: data.fileName,
                     length: data.lengthOfFile
                 });
-                channels.findOne({
+                channels.findOne({//find channel that series is on
                         _id: doc.channelId
                     }, function(err, docchannel) {
                         if (!err) {
-                            var availableTimeslots = [];
-                            var range = ceil((doc.score / 100))
-                            timelines.find({
+                            var availableTimeslots = [];//array that will hold ids of available timeslots
+                            var range = ceil((doc.score / 100));//normalize score into rounded up integer from 1-12
+                            timelines.find({ //ensure that timelines only appear if they have the specified popularity range and are on the right channel
                                 $and: [{
                                     channelId: docchannel._id
                                 }, {
                                     range: range
                                 }]
-                            }).sort({
+                            }).sort({//sort ascending from upcoming dates
                                 dateStarted: "1"
                             }).exec(function(err, doctimelines) {
-                                    if (doctimelines.length == 0) {
+                                    if (doctimelines.length == 0) {//if there are no timelines yet
 
-                                        var ts = new timeslots({
+                                        var ts = new timeslots({//create a new timeslot for the file
                                             start: 0,
                                             end: data.lengthOfFile,
                                             filename: data.fileName
                                         });
-                                        var newTimeline = new timeline({
+                                        var newTimeline = new timeline({//create a new timeline and insert the new timeslot
                                             dateStart: Date.now,
                                             timeslots: [ts],
                                             channelId: docchannel._id,
                                             range: range
                                         });
 
-                                        newTimeline.save();
-                                    } else {
-                                        for (var i = 0; i < doctimelines.length; i++) { //check if anything is left
+                                        newTimeline.save();//save the new timeslot
+                                        availableTimeslots.pust(ts._id);
+                                    } else {//if there are timelines available
+                                        for (var i = 0; i < doctimelines.length; i++) { //loop through each timeline for this range
 
-                                            var lastTime = doctimelines[i].timeslots[doctimelines[i].timeslots.length - 1];
-                                            if ((60 - lastTime) > data.lengthOfFile) {
-                                                var newTimeslot = new timeslots({
+                                            var lastTime = doctimelines[i].timeslots[doctimelines[i].timeslots.length - 1];//find last time segment available in line
+                                            if ((60 - lastTime) > data.lengthOfFile) {//if the show can be inserted into the timeline
+                                                var newTimeslot = new timeslots({//create a new timeslot, insert the show, and append it to the timeslots array
                                                     start: lastTime,
                                                     end: (lastTime + data.lengthOfFile),
                                                     filename: data.fileName
                                                 });
+                                                //update timeline object with new timeslot
                                                 var updatedTimeslot = doctimelines[i].timeslots.push(newTimeslot);
                                                 doctimelines[i].timeslots = updatedTimeslot;
                                                 doctimelines.save();
-
+                                                availableTimeslots.pust(newTimeslot._id);//push available timeslot to arr
                                             }
-                                            // for(var t = 0;t < doctimelines[i].timeslots.length; t=t+2){
-                                            // 	var slot1, slot2;
-                                            // 	slot1 = doctimelines[i].timeslots[t];
-                                            // 	slot2 = doctimelines[i].timeslots[(t+1)];
-                                            // 	if((slot1.end - slot2.start) > data.lengthOfFile){
-                                            // 		var newTimeslot = new timeslots({
-                                            // 			start: slot1.end,
-                                            // 			end: (slot1.end + data.lengthOfFile),
-                                            // 			filename: data.fileName
-                                            // 		});
-                                            // 		doctimelines[i].timeslots.push(newTimeslot);
-                                            // 		availableTimeslots.push(newTimeslot.id);
-                                            // 	}
-                                            // }
                                         }
                                     }});
                                 } else {
